79ee25d23faf1e09f505e6a4994f39d507c031f3
kinanharba <harbakinan@gmail.com>
2024-06-15 17:27:38 +0200

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Registrieren from "./Component/RegistrierenPages/Registrieren";
+import Registrieren from './Component/RegistrierenPages/Registrieren'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
